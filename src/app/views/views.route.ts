1016cae63dcd732f6aca2718641ef7b597b4d603
MedMejri1 <mejrief@gmail.com>
2025-12-11 13:14:16 +0100

    done

--- src/app/views/views.route.ts
+++ src/app/views/views.route.ts
@@ -1,11 +1,13 @@
 import type { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { LandingLayoutComponent } from "@components/landing-layout/landing-layout.component";
+import { AgencyComponent } from "@views/agency/agency.component";
+import { BusinessComponent } from "@views/business/business.component";
 
 export const VIEWS_ROUTES: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: BusinessComponent
   },
   {
     path: 'landing',
