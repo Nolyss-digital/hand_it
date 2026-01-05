ec43e3a2e6402c3669cea67da7b8b6b6ee6fec69
MedMejri1 <mejrief@gmail.com>
2026-01-05 08:38:50 +0100

    done

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare global {
+  interface Window {
+    Calendly: any;
+  }
+}
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
   templateUrl: './contact.component.html',
-  styles: ``
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    this.loadCalendly();
+  }
+
+  loadCalendly() {
+    // si le script est déjà chargé
+    if (window.Calendly) {
+      window.Calendly.initInlineWidgets();
+      return;
+    }
+
+    // sinon on charge le script manuellement
+    const script = document.createElement('script');
+    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    script.async = true;
+    script.onload = () => {
+      window.Calendly.initInlineWidgets();
+    };
+    document.body.appendChild(script);
+  }
 }
